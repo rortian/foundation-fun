3ecda9d848a69ac3b65a9ff0a47c0ac5b36048d5
Jeremy Corbett <jeremy.lael@gmail.com>
2012-12-31 01:10:24 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoundationFun::Application.config.secret_token = 'a91991e4a725cc4615ca8fc078117bd53fcf577daa04e7ab8c7d69d64a615904e85b989375cb7539383536ad2b5000667e40e5712e15c8d13a7b2ab527c9ca34'
