ce6c3804b6e17c980ccb1e24725d7314f6047812
Jeremy Corbett <jeremy.lael@gmail.com>
2013-01-02 01:13:22 -0500

    Some weirdness

--- app/assets/javascripts/svg.js
+++ app/assets/javascripts/svg.js
@@ -1,10 +1,12 @@
 var tessellations = [
-  "cm","cmm","p1","p2","p3"
+  "cm","cmm","p1","p2","p3","p3m1","p4","p4g","p4m","p6"
 ]
 
 angular.module('SvgApp', [], function($routeProvider){
+  for (tess in tessellations){
+    $routeProvider.when("/"+tess,{templateUrl:"/tessellations/"+tess+".html"})
+  }
   $routeProvider
-    .when('/p6',{templateUrl:'/tessellation/p6.html'})
     .otherwise({redirectTo:'p6'})
 })
 
